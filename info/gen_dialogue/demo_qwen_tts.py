8bc9603185eefcf20d30054808d92283973d8d1c
laurencecwj <laurence@c12.ai>
2026-02-01 20:36:35 +0800

    .

---
+++ info/gen_dialogue/demo_qwen_tts.py
@@ -0,0 +1,16 @@
+# 请安装 DashScope SDK 的最新版本
+import os
+import dashscope
+
+# 以下为北京地域url，若使用新加坡地域的模型，需将url替换为：https://dashscope-intl.aliyuncs.com/api/v1
+dashscope.base_http_api_url = 'https://dashscope.aliyuncs.com/api/v1'
+
+text = "那我来给大家推荐一款T恤，这款呢真的是超级好看，这个颜色呢很显气质，而且呢也是搭配的绝佳单品，大家可以闭眼入，真的是非常好看，对身材的包容性也很好，不管啥身材的宝宝呢，穿上去都是很好看的。推荐宝宝们下单哦。"
+response = dashscope.MultiModalConversation.call(
+    model="qwen3-tts-flash",
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    text=text,
+    voice="Cherry",
+    language_type="Chinese"
+)
+print(response)
